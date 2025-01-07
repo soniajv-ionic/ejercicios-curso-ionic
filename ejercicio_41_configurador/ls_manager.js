7f28e24cf5789157564274ee0fadff49af29e4dd
soniajv-ionic <soniajv.ionic@gmail.com>
2025-01-07 22:36:55 +0100

    update ejercicios 07/01/2025

--- ejercicio_41_configurador/ls_manager.js
+++ ejercicio_41_configurador/ls_manager.js
@@ -1,20 +1,24 @@
-function saveToLocalStorage(key, value) {
-    localStorage.setItem(key, value);
-}
-
-function getFromLocalStorage(key) {
-    return localStorage.getItem(key);
-}
-
 const KEY_TEXT = "text-color";
 const KEY_BG_COLOR = "bg-color";
-let bgColorControl = document.querySelector("#i-bg-color").value;
-let textColorControl = document.querySelector("#i-text-color").value;  
-const bSave = document.querySelector("#b-save");
+const bgColorControl = document.querySelector("#i-bg-color");
+const textColorControl = document.querySelector("#i-text-color");
 const contenedor = document.querySelector("#contenedor");
+const bSave = document.querySelector("#b-save");
 
-bSave.addEventListener('click', () => {
-    saveToLocalStorage.setItem(KEY_BG_COLOR, bgColorControl);
-    saveToLocalStorage.setItem(KEY_TEXT, textColorControl);
-})
+bSave.addEventListener("click", function () {
+  localStorage.setItem(KEY_BG_COLOR, bgColorControl.value);
+  localStorage.setItem(KEY_TEXT, textColorControl.value);
+});
 
+let bgColor = localStorage.getItem(KEY_BG_COLOR);
+let textColor = localStorage.getItem(KEY_TEXT);
+
+if (bgColor) {
+  contenedor.style.backgroundColor = bgColor;
+  bgColorControl.value = bgColor;
+}
+
+if (textColor) {
+  contenedor.style.color = textColor;
+  textColorControl.value = textColor;
+}
---
+++ ejercicio_43_settimeout/script.js
@@ -0,0 +1,24 @@
+/*
+    setTimeout(() => {
+        document.querySelector('footer').style.display = 'none';
+    }, 3000);
+    */
+
+setTimeout(() => {
+  document.querySelector("footer").style.bottom = "-200px";
+}, 3000);
+
+/*
+     setTimeout(() => {
+         document.querySelector('footer').style.position = 'static';
+     }, 3000);
+     */
+const scaryFace = document.querySelector(".scary-face");
+const audio = new Audio("terror-scream.mp3");
+audio.volume = 0.8; // Volumen al 80%
+scaryFace.classList.add("hidden"); // Añade la clase que oculta el div
+
+setTimeout(() => {
+  scaryFace.classList.remove("hidden");
+  audio.play();
+}, 5000);
