09fd5dcb8182231dcff95777bec2555723581711
soniajv-ionic <soniajv.ionic@gmail.com>
2025-01-10 00:02:35 +0100

    update ejercicios 09/01/2025

--- ejercicio_49_depuracion/calculadora.js
+++ ejercicio_49_depuracion/calculadora.js
@@ -1,41 +1,39 @@
+const bSumar = document.querySelector(".b-sumar");
+const bRestar = document.querySelector(".b-restar");
+const bLimpiar = document.querySelector(".b-limpiar");
+const num1 = document.querySelector("#num1");
+const num2 = document.querySelector("#num2");
+const result = document.querySelector(".result");
+
 function sumar(num1, num2) {
-
-        if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-            throw new Error('Los argumentos deben ser números');
-        } 
-
-        return num1 + num2;
-
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    throw new Error("Los argumentos deben ser números");
+  }
+  return num1 + num2;
 }
 
 function restar(num1, num2) {
-
-    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-        throw new Error('Los argumentos deben ser números');
-    } 
-
-    return num1 + num2;
-
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    throw new Error("Los argumentos deben ser números");
+  }
+  return num1 + num2;
 }
 
-const bSumar = document.querySelector(".b-sumar");
-const bRestar = document.querySelector(".b-restar");
-
-
 bSumar.addEventListener("click", () => {
-    const num1 = document.querySelector(".num1").value;
-    const num2 = document.querySelector(".num2").value;
-
-    const resultado = sumar(num1, num2);
-
-    document.querySelector(".result").textContent = resultado;
+  result.textContent = `Resultado: ${sumar(
+    Number(num1.value),
+    Number(num2.value)
+  )}`;
 });
 
 bRestar.addEventListener("click", () => {
-    const num1 = document.querySelector(".num1").value;
-    const num2 = document.querySelector(".num2").value;
+  result.textContent = `Resultado: ${restar(
+    Number(num1.value),
+    Number(num2.value)
+  )}`;
+});
 
-    const resultado = restar(num1, num2);
-
-    document.querySelector(".result").textContent = resultado;
+bLimpiar.addEventListener("click", () => {
+  num1.value = "";
+  num2.value = "";
 });
